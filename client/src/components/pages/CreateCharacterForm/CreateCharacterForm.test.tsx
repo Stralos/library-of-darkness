6e06e0e8f644f9306e434b7ba5244e706f159d5e
Paulius Kiselis <paulius.kiselis@wahanda.com>
2020-07-06 14:18:38 +0300

    Add attribute inputs to Character creation form.

--- client/src/components/pages/CreateCharacterForm/CreateCharacterForm.test.tsx
+++ client/src/components/pages/CreateCharacterForm/CreateCharacterForm.test.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { CreateCharacterForm } from './CreateCharacterForm';
+import { render, act, fireEvent } from '@testing-library/react';
+import { CreateCharacterForm, Props } from './CreateCharacterForm';
 
 describe('CreateCharacterForm', () => {
-  it('should trigger submit when form is submitted', () => {
+  const setup = (props: Props) => {
+    const helpers = render(<CreateCharacterForm {...props} />);
+    return {
+      ...helpers,
+      updateField: (testId: string, value: string) =>
+        fireEvent.change(helpers.getByTestId(testId), {
+          target: { value: value },
+        }),
+    };
+  };
 
-  })
-})
+  it('should trigger submit when form is submitted', async () => {
+    const props = {
+      onSubmit: jest.fn(),
+    };
+    const formValues = {
+      vice: 'sloth',
+      virtue: 'holy',
+      clan: 'Ventrue',
+      legacy: 'Malkavian',
+      concept: 'Mad docktor',
+      covenant: 'Ordo',
+      attributes: {
+        intelligence: 3,
+      },
+    };
+    const { getAllByTestId, getByTestId, updateField } = setup(props);
+    updateField('input-vice', formValues.vice);
+    updateField('input-virtue', formValues.virtue);
+    updateField('input-clan', formValues.clan);
+    updateField('input-legacy', formValues.legacy);
+    updateField('input-concept', formValues.concept);
+    updateField('input-covenant', formValues.covenant);
+
+    getAllByTestId('input-intelligence')
+      .find(
+        (input) =>
+          Number(input.getAttribute('value')) ===
+          formValues.attributes.intelligence
+      )
+      ?.click();
+    await act(async () => getByTestId('submit-form').click());
+
+    expect(props.onSubmit).toHaveBeenCalledWith(formValues);
+  });
+});
--- client/src/components/pages/CreateCharacterForm/CreateCharacterForm.tsx
+++ client/src/components/pages/CreateCharacterForm/CreateCharacterForm.tsx
@@ -3,35 +3,51 @@ import { Form, Input, InputGroup, Radio } from 'components/common/Form';
 import style from './CreateCharacterForm.module.scss';
 
 interface Character {
-  name: string;
-  vice: string;
-  attributes: {
-    intelligence: number;
+  name?: string;
+  virtue?: string;
+  vice?: string;
+  concept?: string;
+  clan?: string;
+  legacy?: string;
+  covenant?: string;
+  attributes?: {
+    intelligence?: number;
   };
 }
 
-export const CreateCharacterForm = () => {
+export interface Props {
+  character?: Character;
+  onSubmit: (props: Character) => void;
+}
+
+export const CreateCharacterForm = (props: Props) => {
+  const { onSubmit, character } = props;
   return (
     <Form<Character>
-      onSubmit={(data) => {
-        console.log(data);
-      }}
-      defaultValues={{
-        vice: 'sloth',
-      }}
+      onSubmit={onSubmit}
+      defaultValues={character}
     >
       {({ watch, setValue }) => (
         <Fragment>
-          <div> Test form</div>
-          <Input name="vice" />
+          <Input data-testid="input-name" name="name"/>
+          <Input data-testid="input-virtue" name="virtue"/>
+          <Input data-testid="input-vice" name="vice" />
+          <Input data-testid="input-clan" name="clan"/>
+          <Input data-testid="input-legacy" name="legacy"/>
+          <Input data-testid="input-concept" name="concept"/>
+          <Input data-testid="input-covenant" name="covenant"/>
+
           <InputGroup
             render={({ value, ...rest }) => (
               <div className={style.attributeSelection}>
+                <Radio {...rest} value={5} checked={value === 5} />
+                <Radio {...rest} value={4} checked={value === 4} />
                 <Radio {...rest} value={3} checked={value === 3} />
                 <Radio {...rest} value={2} checked={value === 2} />
                 <Radio {...rest} value={1} checked={value === 1} />
               </div>
             )}
+            data-testid="input-intelligence"
             onChange={([e]) => Number(e.target.value)}
             onClick={(e) => {
               if (e.target.checked) {
@@ -40,7 +56,9 @@ export const CreateCharacterForm = () => {
             }}
             name="attributes.intelligence"
           />
-          <button type="submit"> Submit</button>
+          <button data-testid="submit-form" type="submit">
+            Submit
+          </button>
           <pre style={{ whiteSpace: 'pre-wrap' }}>
             {JSON.stringify(watch())}
           </pre>
