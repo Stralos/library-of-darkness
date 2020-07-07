38d9a41f5e136245690fe2d6d9529089210e9048
Paulius Kiselis <paulius.kiselis@wahanda.com>
2020-07-07 09:45:31 +0300

    refactor App to be an arrow function named export.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { CreateCharacterForm } from 'components/pages/CreateCharacterForm';
 
-function App() {
+export const App = () => {
   return (
     <div>
       <div>Create Character form</div>
       <CreateCharacterForm onSubmit={alert} />
     </div>
   );
-}
-
-export default App;
+};
