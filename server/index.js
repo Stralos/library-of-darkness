7d58b916d46fdb59dd7f71971915cc912ec3935f
Stralos <stralosthedev@gmail.com>
2020-09-08 15:36:38 +0300

    Add helmet and dev mode to the server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 
 const port = 3010;
 const app = express();
 
+app.use(helmet());
 app.use(morgan("combined"));
 app.get("/greet", (req, res) => {
   res.send({ message: "Hello world from the server!" });
